c11f98cccaeefdd816b111ca7b3577872376dab9
juanfu7467v <movistarcervise@gmail.com>
2025-09-10 08:20:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import axios from "axios";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 const TOKEN = process.env.TOKEN;
 
 app.use(cors());
